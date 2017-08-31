f04f32e7b292e8b2476a4bc5d9840881dc4cf7be
Parris Khachi <parris@letswoosh.com>
2017-08-30 19:37:10 -0700

    Fix tests - Partially Upgraded Enzyme

--- test/unit/jestSetup.js
+++ test/unit/jestSetup.js
@@ -1,5 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies, import/first */
 import fetch from 'jest-fetch-mock';
 import 'react-dom';
+import { configure } from 'enzyme';
+import Adapter from './helpers/react16adapter';
+
+configure({ adapter: new Adapter() });
 
 global.fetch = fetch;
