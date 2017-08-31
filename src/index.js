59db1d41dc1b053b99294d6d4d482821a20147db
Parris Khachi <parris@letswoosh.com>
2017-08-31 09:59:07 -0700

    Moved library exports
    It was a bit ambiguous previously what would happen if you
    imported 'fervor'. You now need to import 'fervor/lib' to
    get libraries and components to your code.

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import { compose, gql as apolloGQL, graphql } from 'react-apollo';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
+
+import Document from './client/components/Document';
+import Form from './client/components/Form';
+
+module.exports = {
+  compose,
+  connect,
+  // NOTE: I don't like this function living here.
+  // If it happens again let's find it a new home.
+  gql: (str) => {
+    const ast = apolloGQL(str);
+    ast.query = str;
+    return ast;
+  },
+  graphql,
+  Link,
+  PropTypes,
+  React,
+
+  Document,
+  Form,
+};
