a332100001920b300307a1034ee80e2e9348e4ba
Parris Khachi <parris@letswoosh.com>
2017-08-31 17:08:58 -0700

    Dev shouldn't have a [hash] for its bundle name

--- src/config/webpack.dev.js
+++ src/config/webpack.dev.js
@@ -34,7 +34,7 @@ export default (app, options) => {
       output: {
         path: options.appLocation,
         publicPath: '/build/',
-        filename: 'bundle-[hash:6].js',
+        filename: 'bundle.js',
         sourceMapFilename: 'bundle.js.map',
       },
       devtool: 'inline-source-map',
