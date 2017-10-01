4e9dd349fd236ccfc8b5f2a955cfb3bbe8717634
Parris Khachi <parris@letswoosh.com>
2017-10-01 14:46:34 -0700

    onSuccess and onFailure tests

--- test/unit/client/Form.spec.js
+++ test/unit/client/Form.spec.js
@@ -69,3 +69,105 @@ describe('Form Component', () => {
     });
   });
 });
+
+describe('Form Component, when onSuccess is defined', () => {
+  let mutateStub;
+  let successStub;
+  let completeStub;
+  let component;
+
+  beforeEach(() => {
+    const webClient = new ApolloClient({
+      networkInterface: createNetworkInterface({
+        uri: '/graphql',
+      }),
+    });
+    const store = initStore({});
+
+    const mutation = gql`query {
+      __schema {
+        types {
+          name
+          description
+        }
+      }
+    }`;
+
+    mutateStub = jest.fn(() => new Promise((resolve) => resolve()));
+    completeStub = new Promise((resolve) => {
+      successStub = jest.fn(() => {
+        resolve();
+      });
+    });
+
+    component = mount(
+      <ApolloProvider client={webClient} store={store}>
+        <Form mutation={mutation} mutate={mutateStub} onSuccess={successStub}>
+          <div>Hello World</div>
+          <input name="oh" defaultValue="world" />
+        </Form>
+      </ApolloProvider>,
+    );
+  });
+
+  it('triggers the onSuccess hook', async () => {
+    try {
+      component.find('form').simulate('submit');
+    } catch (e) {
+      // remove try catch, once enzyme 3 is released
+    }
+    await completeStub;
+    expect(successStub.mock.calls.length).toEqual(1);
+  });
+});
+
+describe('Form Component, when onFailure is specified', () => {
+  let mutateStub;
+  let failureStub;
+  let completeStub;
+  let component;
+
+  beforeEach(() => {
+    const webClient = new ApolloClient({
+      networkInterface: createNetworkInterface({
+        uri: '/graphql',
+      }),
+    });
+    const store = initStore({});
+
+    const mutation = gql`query {
+      __schema {
+        types {
+          name
+          description
+        }
+      }
+    }`;
+
+    mutateStub = jest.fn(() => new Promise((resolve) => resolve()));
+    completeStub = new Promise((resolve) => {
+      failureStub = jest.fn(() => {
+        resolve();
+      });
+    });
+
+    component = mount(
+      <ApolloProvider client={webClient} store={store}>
+        <Form mutation={mutation} mutate={mutateStub} onFailure={failureStub}>
+          <div>Hello World</div>
+          <input name="oh" defaultValue="world" />
+        </Form>
+      </ApolloProvider>,
+    );
+  });
+
+  it('triggers the onFailure hook', async () => {
+    try {
+      component.find('form').simulate('submit');
+    } catch (e) {
+      // remove try catch, once enzyme 3 is released
+    }
+    await completeStub;
+    expect(failureStub.mock.calls.length).toEqual(1);
+  });
+});
