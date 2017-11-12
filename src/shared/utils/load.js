7144be22f888e3dc3c511d5f07be4b1aff05bd75
Michael Hahn <mh@slack-corp.com>
2017-11-12 13:12:15 -0800

    Don't rely on `fs`

--- src/shared/utils/load.js
+++ src/shared/utils/load.js
@@ -1,30 +1,31 @@
-import fs from 'fs';
-
 export default function load(
   name,
   { args = undefined, options = {}, default: _default = null },
 ) {
-  let module = _default;
-  if (
-    options.disableWebpack &&
-    (fs.existsSync(`${options.appLocation}/build/${name}.js`) ||
-      fs.existsSync(`${options.appLocation}/build/${name}/index.js`))
-  ) {
+  let module;
+  try {
     // eslint-disable-next-line global-require, import/no-dynamic-require
     module = require(`${options.appLocation}/build/${name}`);
-  } else if (
-    fs.existsSync(`${options.appLocation}/src/${name}.js`) ||
-    fs.existsSync(`${options.appLocation}/src/${name}/index.js`)
-  ) {
-    // eslint-disable-next-line global-require, import/no-dynamic-require
-    module = require(`${options.appLocation}/src/${name}`);
+  } catch (buildErr) {
+    try {
+      // eslint-disable-next-line global-require, import/no-dynamic-require
+      module = require(`${options.appLocation}/src/${name}`);
+    } catch (srcErr) {
+      module = undefined;
+    }
   }
 
   let res;
-  if (args !== undefined) {
-    res = module.default(args);
-  } else {
-    res = module.default();
+  if (module !== undefined) {
+    if (args !== undefined) {
+      res = module.default(args);
+    } else {
+      res = module.default();
+    }
+  }
+
+  if (res === undefined) {
+    res = _default;
   }
 
   return res;
