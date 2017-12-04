b87fd42e2eae76d9be42148259f7ed44f80a40dd
Parris Khachi <parris@letswoosh.com>
2017-12-03 18:21:38 -0800

    Async/Await Client Side Test
    This makes sure that async/await works on the client side. It is
    something that's easy to break and requires polyfills.

--- test/integration/prod/testApp/src/urls.js
+++ test/integration/prod/testApp/src/urls.js
@@ -1,7 +1,9 @@
 import Hello from './apps/Hello';
 import Hello2 from './apps/Hello2';
+import Test from './apps/Test';
 
 export default {
   '/': Hello,
   '/hello2': Hello2,
+  '/test': Test,
 };
