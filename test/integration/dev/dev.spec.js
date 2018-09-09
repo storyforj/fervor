c55735fb46cb7391a912be1d9be8765ee5fe8ccb
Parris Khachi <parris@storyforj.com>
2018-09-09 10:56:08 -0700

    Simple lint clean up

--- test/integration/dev/dev.spec.js
+++ test/integration/dev/dev.spec.js
@@ -29,13 +29,13 @@ describe('Dev server', () => {
     browser.url('http://localhost:3002/');
     browser.waitUntil(() => (
       browser
-      .getCssProperty('div[class*="component"]', 'background')
-      .value.indexOf('rgb(0,0,0)') >= -1
+        .getCssProperty('div[class*="component"]', 'background')
+        .value.indexOf('rgb(0,0,0)') >= -1
     ), 10000);
     browser.waitUntil(() => (
       browser
-      .getCssProperty('div[class*="component"]', 'color')
-      .value.indexOf('rgba(255,255,255,1)') >= -1
+        .getCssProperty('div[class*="component"]', 'color')
+        .value.indexOf('rgba(255,255,255,1)') >= -1
     ), 10000);
   });
 
