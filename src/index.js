0346ca53db3a72ebefa9e0d8ce6ace04fc2810b7
Parris Khachi <parris@storyforj.com>
2020-01-03 14:13:47 -0800

    Expose Redirect from ReactRouterDom
    It seems that you MUST use the same instance of ReactRouterDom through
    out your application. I'm guessing it is "singleton" of some sort or
    the context gets lost somehow if you access it from 2 separate places.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import clientCookies from 'cookies-js';
 import compose from 'lodash.flowright';
 import { Query, Mutation, Subscription, graphql } from 'react-apollo';
 import { Helmet } from 'react-helmet';
-import { Link, Switch, Route } from 'react-router-dom';
+import { Link, Switch, Redirect, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Provider, connect } from 'react-redux';
@@ -31,6 +31,7 @@ module.exports = {
   Subscription,
   Provider,
   Link,
+  Redirect,
   Route,
   Switch,
   graphql,
